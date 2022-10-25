8cce23833d52dcd4dad29af7989c060979dfd908
yunghaze <florian.langevillier-seranon@epitech.eu>
2022-10-25 10:38:33 +0200

    WorkingTime Controller
    Router.ex

--- lib/time_manager_web/router.ex
+++ lib/time_manager_web/router.ex
@@ -8,6 +8,7 @@ defmodule TimeManagerWeb.Router do
   scope "/api", TimeManagerWeb do
     pipe_through :api
     resources "/users", UserController
+    resources "/working_times", WorkingTimeController
   end
 
   # Enables LiveDashboard only for development
