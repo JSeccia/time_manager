8a3354d5d657a2387fb22030caeb7c1755776c0a
anna8321 <84349590+anna8321@users.noreply.github.com>
2022-10-26 19:36:45 +0200

    create workingtimes component and working times view

--- front/web/time-manager/src/App.vue
+++ front/web/time-manager/src/App.vue
@@ -1,29 +1,26 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+ 
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="You did it!" /> -->
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/workingtimes">WorkingTimes</RouterLink>
+
       </nav>
     </div>
   </header>
-
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped>
--- front/web/time-manager/src/router/index.js
+++ front/web/time-manager/src/router/index.js
@@ -17,6 +17,12 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
+    // WorkingTimes route
+    {
+      path: "/workingtimes",
+      name: "WorkingTimes",
+      component: () => import("../views/WorkingTimesView.vue"),
+    },
   ],
 });
 
