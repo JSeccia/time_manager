a169184dba878e6d54956bdeebc8aec84b40bffb
anna8321 <84349590+anna8321@users.noreply.github.com>
2022-10-27 15:19:28 +0200

    adding alert if working time or user id entered isn't found

--- front/web/time-manager/src/components/WorkingTimes.vue
+++ front/web/time-manager/src/components/WorkingTimes.vue
@@ -1,48 +1,60 @@
 <template>
-    <form @submit="getWorkingTimes">
-    <input type="text" id="get_user_working_times" v-model="userId" />
+  <br>
+  <form @submit="getWorkingTimes">
     <label for="get_user_working_times" placeholder="enter employee id"></label>
-    <input type="submit" value="Get user working times" />
+    <input
+      type="text"
+      id="get_user_working_times"
+      v-model="userId"
+      placeholder="enter employee id"
+      ref="userIdInput"
+    />
+    <input type="submit" value="Get working times" />
   </form>
-  <ul class="item" v-for="item in workingTimes" :key="item.id">
-        <li>{{item.id}}</li>
-        <li>{{item.userId}}</li>
-        <li>{{item.start}}</li>
-        <li>{{item.end}}</li>
-      </ul>
-   
-  </template>
+  <br>
+  <h2>Employee {{ currentUserId }} Working Times:</h2>
+  <ul class="WT_item" v-for="item in workingTimes" :key="item.id">
+    <p>Working Times number {{ item.id }}</p>
+    <li>Start work time: {{ item.start }}</li>
+    <li>End work time:{{ item.end }}</li>
+    <br>
+  </ul>
+
+</template>
 
 <script>
-  import axios from "axios";
+import axios from "axios";
 
-  export default {
-    name: "WorkingTimes",
-    data()
-    {
-      return {
-        workingTimes: {
-          start: "",
-          end: "",
-        },
-      };
-    },
-    methods: {
-      getWorkingTimes(e) {
-        e.preventDefault();
-        if (this.userID === null) {
-          window.alert("no id");
-          return;
-        }
-        axios
+export default {
+  name: "WorkingTimes",
+  data() {
+
+    return {
+      workingTimes: {
+        // userId: 0,
+      },
+      currentUserId: "",
+    };
+  },
+  methods: {
+    getWorkingTimes(submitNumber) {
+      submitNumber.preventDefault();
+
+      if (this.userId === "") {
+        window.alert("Please enter an employee id");
+        return;
+      }
+      axios
         .get(`http://192.168.76.161:4000/api/working_times/${this.userId}`)
         .then((response) => {
-            // localStorage.setItem("currentUserWT", JSON.stringify(response.data.data));
-            console.log(response.data)
+          if (response.data.data.length > 0) {
             this.workingTimes = response.data.data;
-        })
-      },
-    }, 
-  }
-  
+            this.currentUserId = this.$refs.userIdInput.value;
+          } else {
+            window.alert("No employer or working times found");
+          }
+        });
+    },
+  },
+};
 </script>
