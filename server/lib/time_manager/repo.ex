1e7030ac72add437c40d36b030487f6427f26375
E-M-Cay <82440817+E-M-Cay@users.noreply.github.com>
2022-10-31 10:51:33 +0100

    deployment back ok + data persistance

--- server/lib/time_manager/repo.ex
+++ server/lib/time_manager/repo.ex
@@ -5,11 +5,11 @@ defmodule TimeManager.Repo do
 
   def init(_, config) do
     config = config
-      |> Keyword.put(:username, System.get_env("PGUSER"))
-      |> Keyword.put(:password, System.get_env("PGPASSWORD"))
-      |> Keyword.put(:database, System.get_env("PGDATABASE"))
-      |> Keyword.put(:hostname, System.get_env("PGHOST"))
-      |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer)
+      |> Keyword.put(:username, System.get_env("PGUSER") || "postgres")
+      |> Keyword.put(:password, System.get_env("PGPASSWORD") || "postgres")
+      |> Keyword.put(:database, System.get_env("PGDATABASE") || "time_manager_dev")
+      |> Keyword.put(:hostname, System.get_env("PGHOST") || "localhost")
+      |> Keyword.put(:port, System.get_env("PGPORT") || 5432)
     {:ok, config}
   end
 end
