e8854a79c1d4d32a0856dd90e5614f1b283f0e87
E-M-Cay <82440817+E-M-Cay@users.noreply.github.com>
2022-10-31 17:15:35 +0100

    correction docker + ip

--- front/web/time-manager-temp/src/components/WTComponent.vue
+++ front/web/time-manager-temp/src/components/WTComponent.vue
@@ -1,33 +1,39 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
-    
-    <ul>
-        <li>Number : {{workingTime.id}}</li>
-        <li :key="workingTime.start">start work :{{workingTime.start}}</li>
-        <li :key="workingTime.end"> end work : {{workingTime.end}}</li>
-    </ul>
-    <input type="button" value="update" @click="handleUpdate">
-    <input type="button" value="delete" @click="deleteWorkingTime">
+  <ul>
+    <li>Number : {{ workingTime.id }}</li>
+    <li :key="workingTime.start">start work :{{ workingTime.start }}</li>
+    <li :key="workingTime.end">end work : {{ workingTime.end }}</li>
+  </ul>
+  <input type="button" value="update" @click="handleUpdate" />
+  <input type="button" value="delete" @click="deleteWorkingTime" />
 
-    <form v-if="isUpdateButtonSelected" @submit="updateWorkingTime">
-      <p>Fill this form to update the working time:</p>
-      <label for="start_date_input">Start Time</label>
-      <input type="datetime-local" v-model="StartDate" id="start_date_input" name="start_date_input">
-      <br>
-      <label for="end_date_input">End Time</label>
-      <input type="datetime-local" v-model="EndDate" id="end_date_input" name="end_date_input">
-      <br>
-      <input type="submit" id="update_working_time_submit" value="edit" >
-    </form>
-   
-    
-    <br>
+  <form v-if="isUpdateButtonSelected" @submit="updateWorkingTime">
+    <p>Fill this form to update the working time:</p>
+    <label for="start_date_input">Start Time</label>
+    <input
+      type="datetime-local"
+      v-model="StartDate"
+      id="start_date_input"
+      name="start_date_input"
+    />
+    <br />
+    <label for="end_date_input">End Time</label>
+    <input
+      type="datetime-local"
+      v-model="EndDate"
+      id="end_date_input"
+      name="end_date_input"
+    />
+    <br />
+    <input type="submit" id="update_working_time_submit" value="edit" />
+  </form>
 
+  <br />
 </template>
 <!-- eslint-disable prettier/prettier -->
 
 <script>
-
 import axios from "axios";
 // import moment from "moment";
 export default {
@@ -48,7 +54,9 @@ export default {
   methods: {
     getWorkingTime() {
       axios
-        .get(`http://192.168.73.197:4000/api/working_times/${this.userId}/${this.workingTimeId}`)
+        .get(
+          `http://localhost:4000/api/working_times/${this.userId}/${this.workingTimeId}`
+        )
         .then((response) => {
           this.workingTime = response.data.data;
           console.log(this.workingTime);
@@ -61,40 +69,46 @@ export default {
     updateWorkingTime(e) {
       e.preventDefault();
       console.log("update button clicked");
-      
+
       const body = {
         working_time: {
-            user: this.workingTime.user,
-            start: this.StartDate ?? null,
-            end: this.EndDate ?? null,
+          user: this.workingTime.user,
+          start: this.StartDate ?? null,
+          end: this.EndDate ?? null,
         },
-      }
+      };
       axios
-        .put(`http://192.168.73.197:4000/api/working_times/${this.workingTimeId}`, body, {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
+        .put(
+          `http://localhost:4000/api/working_times/${this.workingTimeId}`,
+          body,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
         .then((response) => {
           console.log(response);
-          this.$router.push(`/workingtime/${this.userId}/${this.workingTimeId}`);
+          this.$router.push(
+            `/workingtime/${this.userId}/${this.workingTimeId}`
+          );
           this.$router.go(0);
         });
-        console.log(body);    
+      console.log(body);
     },
     deleteWorkingTime() {
       console.log("delete button clicked");
       if (confirm("Are you sure you want to delete this working time?")) {
-        axios
-          .delete(`http://192.168.73.197:4000/api/working_times/${this.workingTimeId}`)
-          this.$router.go(-1);
+        axios.delete(
+          `http://localhost:4000/api/working_times/${this.workingTimeId}`
+        );
+        this.$router.go(-1);
       }
     },
   },
-  
+
   mounted() {
     this.getWorkingTime();
   },
 };
 </script>
-
