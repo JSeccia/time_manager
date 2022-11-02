e4477c837884bff6807a9f709cf128eff670453e
anna8321 <84349590+anna8321@users.noreply.github.com>
2022-11-02 15:58:59 +0100

    Create LoginComponent.vue

---
+++ front/web/time-manager-temp/src/components/LoginComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <q-page padding>
+        <h1 class="titre">WELCOME TO TIME MANAGER</h1>
+    </q-page>
+</template>
+
+
+<script>
+
+export default {
+    name: "LoginComponent",
+}
+
+</script>
+
+
+<style>
+.titre {
+    text-align: center;
+    font-size: 50px;
+    color: green;
+}
+</style>
