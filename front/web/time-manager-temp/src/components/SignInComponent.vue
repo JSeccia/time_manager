07ccd3b0f380078897747a0ab3792073264ff5c2
yunghaze <florian.langevillier-seranon@epitech.eu>
2022-11-03 12:14:45 +0100

    Adding SignIn page
    Add page for sign in + add route for it in /signin

--- front/web/time-manager-temp/src/components/SignInComponent.vue
+++ front/web/time-manager-temp/src/components/SignInComponent.vue
@@ -5,15 +5,18 @@
                 <p color="green-10">Welcome on Time Manager</p>
             </div>
             <div class="login_section__form">
-                <p>Log In as a Manager !</p>
+                <p>Create your account now !</p>
                 <q-input rounded outlined v-model="text" class="input_login" label="Username" color="green-10">
                 </q-input>
                 <q-input rounded outlined v-model="text" class="input_login" label="Password" color="green-10"
                     type="password">
                 </q-input>
+                <q-input rounded outlined v-model="text" class="input_login" label="Re-type your password"
+                    color="green-10" type="password">
+                </q-input>
                 <q-btn class="input_btn" push color="green-10" label="Connect" />
                 <div class="oubli_psswd">
-                    <p>Forgot your password?</p>
+                    <p>Already got an account ?</p>
                 </div>
             </div>
         </div>
@@ -41,7 +44,7 @@ export default {
 
 .login_section {
     width: 50%;
-    height: 70%;
+    height: 85%;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -65,9 +68,9 @@ export default {
 
 .login_section__form {
     color: black;
-    height: 55%;
+    height: 70%;
     width: 80%;
-    margin-bottom: 10%;
+    margin-bottom: 5%;
     display: flex;
     flex-direction: column;
     align-items: center;
---
+++ front/web/time-manager-temp/src/pages/SignInPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <SignInComponent />
+    </div>
+</template>
+
+
+
+
+
+
+<script>
+
+import { defineComponent } from 'vue';
+import SignInComponent from 'src/components/SignInComponent.vue';
+
+export default defineComponent({
+    name: 'SignInPage',
+
+    components: {
+        SignInComponent,
+    },
+});
+
+</script>
