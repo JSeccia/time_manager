c06f7df5239c94115204a26ce479fef908b18f5b
anna8321 <84349590+anna8321@users.noreply.github.com>
2022-11-04 12:08:24 +0100

    quasar

--- front/web/time-manager-temp/quasar.conf.js
+++ front/web/time-manager-temp/quasar.conf.js
@@ -83,7 +83,7 @@ module.exports = configure(function (/* ctx */) {
       proxy: {
         // proxy all requests starting with /api to jsonplaceholder
         "/api": {
-          target: "http://172.19.34.115:4000",
+          target: "http://localhost:4000",
           changeOrigin: true,
           pathRewrite: {
             "^/api": "",
