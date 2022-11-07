51955bc31f271fca9801cad215e0a35184a88c59
E-M-Cay <jerome.seccia@epitech.eu>
2022-11-07 11:18:27 +0100

    creating wt when clocking out

--- server/lib/time_manager_web/controllers/clock_controller.ex
+++ server/lib/time_manager_web/controllers/clock_controller.ex
@@ -9,25 +9,48 @@ defmodule TimeManagerWeb.ClockController do
   alias TimeManager.Api.User
   alias TimeManager.Api.WorkingTime
 
-  action_fallback TimeManagerWeb.FallbackController
+  action_fallback(TimeManagerWeb.FallbackController)
 
   def index(conn, %{"user_id" => user_id}) do
-    clocks = Repo.all(from c in Clock, where: c.user == ^user_id)
+    clocks = Repo.all(from(c in Clock, where: c.user == ^user_id))
     render(conn, "index.json", clocks: clocks)
   end
 
   def create_by_id(conn, %{"username" => username}) do
-    user_id = Repo.one(from u in User, where: u.username == ^username).id
-    previous_clock = Repo.one(from c in Clock, where: c.user == ^user_id, order_by: [desc: c.id], limit: 1)
-    clock_params = %{}
-    clock_params = Map.put(clock_params, "status", (if previous_clock != nil, do: !previous_clock.status, else: true))
-    clock_params = Map.put(clock_params, "user", user_id)
-    clock_params = Map.put(clock_params, "time", Timex.now())
-    with {:ok, %Clock{} = clock} <- Api.create_clock(clock_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.clock_path(conn, :create_by_id, clock))
-      |> render("show.json", clock: clock)
+    user_id = Repo.one(from(u in User, where: u.username == ^username)).id
+
+    previous_clock =
+      Repo.one(from(c in Clock, where: c.user == ^user_id, order_by: [desc: c.id], limit: 1))
+
+    if previous_clock == nil || previous_clock.status == false do
+      clock_params = %{}
+      clock_params = Map.put(clock_params, "status", true)
+      clock_params = Map.put(clock_params, "user", user_id)
+      clock_params = Map.put(clock_params, "time", Timex.now())
+
+      with {:ok, %Clock{} = clock} <- Api.create_clock(clock_params) do
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", Routes.clock_path(conn, :create_by_id, clock))
+        |> render("show.json", clock: clock)
+      end
+    else
+      working_time_params = %{}
+      working_time_params = Map.put(working_time_params, "start", previous_clock.time)
+      working_time_params = Map.put(working_time_params, "end", Timex.now())
+      working_time_params = Map.put(working_time_params, "user", user_id)
+
+      with {:ok, %WorkingTime{} = working_time} <- Api.create_working_time(working_time_params) do
+        Api.delete_clock(previous_clock)
+
+        conn
+        |> put_status(:created)
+        |> put_resp_header(
+          "location",
+          Routes.working_time_path(conn, :create_by_id, working_time)
+        )
+        |> render("working_time.json", working_time: working_time)
+      end
     end
   end
 
